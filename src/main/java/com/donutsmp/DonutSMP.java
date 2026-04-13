4fb614ca941cfea588346189521820b6acf42ebd
AtlasWon <thatguycreaper@gmail.com>
2026-04-13 13:29:52 -0400

    Update DonutSMP.java

--- src/main/java/com/donutsmp/DonutSMP.java
+++ src/main/java/com/donutsmp/DonutSMP.java
@@ -1,13 +1,9 @@
 package com.donutsmp;
 
 import com.donutsmp.config.DonutConfig;
-import com.donutsmp.event.ClientEvents;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,10 +18,6 @@ public class DonutSMP {
     public DonutSMP() {
         LOGGER.info("[DonutSMP] Initializing...");
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, DonutConfig.SPEC, "donutsmp-client.toml");
-        if (FMLEnvironment.dist == Dist.CLIENT) {
-            MinecraftForge.EVENT_BUS.register(new ClientEvents());
-            LOGGER.info("[DonutSMP] Client events registered.");
-        }
-        LOGGER.info("[DonutSMP] v{} loaded for MC 1.21.6.", MOD_VERSION);
+        LOGGER.info("[DonutSMP] v{} loaded.", MOD_VERSION);
     }
 }
