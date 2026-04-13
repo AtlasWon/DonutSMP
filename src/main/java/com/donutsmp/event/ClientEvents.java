0a7d0bed1aedf0fbb91769f2f973467f6e9e3657
AtlasWon <thatguycreaper@gmail.com>
2026-04-13 13:38:01 -0400

    Update ClientEvents.java

--- src/main/java/com/donutsmp/event/ClientEvents.java
+++ src/main/java/com/donutsmp/event/ClientEvents.java
@@ -20,7 +20,7 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = DonutSMP.MOD_ID, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = DonutSMP.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientEvents {
 
     private static final Particles uniParticles = new Particles();
