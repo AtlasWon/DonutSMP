643e26685f8e053826c3ade65e41e31a226792e0
AtlasWon <thatguycreaper@gmail.com>
2026-04-13 20:45:34 -0400

    Update DonutSMP.java

--- src/main/java/com/donutsmp/DonutSMP.java
+++ src/main/java/com/donutsmp/DonutSMP.java
@@ -3,14 +3,16 @@ package com.donutsmp;
 import com.donutsmp.config.DonutConfig;
 import com.donutsmp.event.ClientEvents;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.BusGroup;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 @Mod(DonutSMP.MOD_ID)
 public class DonutSMP {
@@ -19,18 +21,13 @@ public class DonutSMP {
     public static final String MOD_VERSION = "1.1.0";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-    @SuppressWarnings("removal")
-    public DonutSMP() {
+    public DonutSMP(FMLJavaModLoadingContext context) {
         LOGGER.info("[DonutSMP] Initializing...");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, DonutConfig.SPEC, "donutsmp-client.toml");
+        context.registerConfig(ModConfig.Type.CLIENT, DonutConfig.SPEC, "donutsmp-client.toml");
         if (FMLEnvironment.dist == Dist.CLIENT) {
-            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+            BusGroup.DEFAULT.register(MethodHandles.lookup(), ClientEvents.class);
+            LOGGER.info("[DonutSMP] Client events registered.");
         }
         LOGGER.info("[DonutSMP] v{} loaded.", MOD_VERSION);
-    }
-
-    private void onClientSetup(FMLClientSetupEvent event) {
-        ClientEvents.register();
-        LOGGER.info("[DonutSMP] Client setup complete.");
     }
 }
